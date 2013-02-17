e7533fded10d0074b9a507714776bf0e08713ef9
David Harris <harry491@gmail.com>
2013-02-16 18:47:55 -0800

    add a working as.is argument to melt
    Default is TRUE

--- R/melt.r
+++ R/melt.r
@@ -15,8 +15,9 @@
 #'   convert explicit missings to implicit missings.
 #' @param ... further arguments passed to or from other methods.
 #' @param value.name name of variable used to store values
+#' @param as.is should strings be left as strings or converted to factors?
 #' @export
-melt <- function(data, ..., na.rm = FALSE, value.name = "value") {
+melt <- function(data, ..., na.rm = FALSE, value.name = "value", as.is = TRUE) {
   UseMethod("melt", data)
 }
 
@@ -58,18 +59,18 @@ melt.default <- function(data, ..., na.rm = FALSE, value.name = "value") {
 melt.list <- function(data, ..., level = 1) {
   parts <- lapply(data, melt, level = level + 1, ...)
   result <- rbind.fill(parts)
-
+  
   # Add labels
   names <- names(data) %||% seq_along(data)
   lengths <- vapply(parts, nrow, integer(1))
   labels <- rep(names, lengths)
-
+  
   label_var <- attr(data, "varname") %||% paste("L", level, sep = "")
   result[[label_var]] <- labels
-
+  
   # result <- cbind(labels, result)
   # result[, c(setdiff(names(result), "value"), "value")]
-
+  
   result
 }
 
@@ -92,6 +93,7 @@ melt.list <- function(data, ..., level = 1) {
 #' @param value.name name of variable used to store values
 #' @param na.rm Should NA values be removed from the data set? This will
 #'   convert explicit missings to implicit missings.
+#' @param as.is should strings be left as strings or converted to factors?
 #' @param ... further arguments passed to or from other methods.
 #' @keywords manip
 #' @method melt data.frame
@@ -101,25 +103,27 @@ melt.list <- function(data, ..., level = 1) {
 #' melt(airquality, id=c("month", "day"))
 #' names(ChickWeight) <- tolower(names(ChickWeight))
 #' melt(ChickWeight, id=2:4)
-melt.data.frame <- function(data, id.vars, measure.vars, variable.name = "variable", ..., na.rm = FALSE, value.name = "value") {
+melt.data.frame <- function(data, id.vars, measure.vars, variable.name = "variable", ..., na.rm = FALSE, value.name = "value", as.is = TRUE) {
   var <- melt_check(data, id.vars, measure.vars)
-
+  
   ids <- unrowname(data[, var$id, drop = FALSE])
   if (length(var$measure) == 0) {
     return(ids)
   }
-
+  
   # Turn factors to characters
   factors <- vapply(data, is.factor, logical(1))
   data[factors] <- lapply(data[factors], as.character)
-
+  
   value <- unlist(unname(data[var$measure]))
-  variable <- factor(rep(var$measure, each = nrow(data)),
-    levels = var$measure)
-
+  
+  variable <- rep(var$measure, each = nrow(data))
+  if(!as.is){
+    variable <- factor(variable, levels = var$measure)
+  }
   df <- data.frame(ids, variable, value, stringsAsFactors = FALSE)
   names(df) <- c(names(ids), variable.name, value.name)
-
+  
   if (na.rm) {
     subset(df, !is.na(value))
   } else {
@@ -137,6 +141,7 @@ melt.data.frame <- function(data, id.vars, measure.vars, variable.name = "variab
 #' @param value.name name of variable used to store values
 #' @param na.rm Should NA values be removed from the data set? This will
 #'   convert explicit missings to implicit missings.
+#' @param as.is should strings be left as strings or converted to factors?
 #' @keywords manip
 #' @S3method melt table
 #' @S3method melt matrix
@@ -152,20 +157,26 @@ melt.data.frame <- function(data, id.vars, measure.vars, variable.name = "variab
 #' melt(a, varnames=c("X","Y","Z"))
 #' dimnames(a)[1] <- list(NULL)
 #' melt(a)
-melt.array <- function(data, varnames = names(dimnames(data)), ..., na.rm = FALSE, value.name = "value") {
-  var.convert <- function(x) if(is.character(x)) type.convert(x) else x
-
+melt.array <- function(data, varnames = names(dimnames(data)), ..., na.rm = FALSE, value.name = "value", as.is = TRUE) {
+  var.convert <- function(x){
+    if(is.character(x)){
+      type.convert(x, as.is = as.is)
+    }else {
+      x
+    }
+  }
+  
   dn <- amv_dimnames(data)
   names(dn) <- varnames
   labels <- expand.grid(lapply(dn, var.convert), KEEP.OUT.ATTRS = FALSE,
-    stringsAsFactors = FALSE)
-
+                        stringsAsFactors = FALSE)
+  
   if (na.rm) {
     missing <- is.na(data)
     data <- data[!missing]
     labels <- labels[!missing, ]
   }
-
+  
   value_df <- setNames(data.frame(as.vector(data)), value.name)
   cbind(labels, value_df)
 }
@@ -187,7 +198,7 @@ melt.matrix <- melt.array
 #' @return a list giving id and measure variables names.
 melt_check <- function(data, id.vars, measure.vars) {
   varnames <- names(data)
-
+  
   # Convert positions to names
   if (!missing(id.vars) && is.numeric(id.vars)) {
     id.vars <- varnames[id.vars]
@@ -195,7 +206,7 @@ melt_check <- function(data, id.vars, measure.vars) {
   if (!missing(measure.vars) && is.numeric(measure.vars)) {
     measure.vars <- varnames[measure.vars]
   }
-
+  
   # Check that variables exist
   if (!missing(id.vars)) {
     unknown <- setdiff(id.vars, varnames)
@@ -204,7 +215,7 @@ melt_check <- function(data, id.vars, measure.vars) {
       stop("id variables not found in data: ", vars, call. = FALSE)
     }
   }
-
+  
   if (!missing(measure.vars)) {
     unknown <- setdiff(measure.vars, varnames)
     if (length(unknown) > 0) {
@@ -212,7 +223,7 @@ melt_check <- function(data, id.vars, measure.vars) {
       stop("measure variables not found in data: ", vars, call. = FALSE)
     }
   }
-
+  
   # Fill in missing pieces
   if (missing(id.vars) && missing(measure.vars)) {
     discrete <- sapply(data, is.discrete)
@@ -224,6 +235,6 @@ melt_check <- function(data, id.vars, measure.vars) {
   } else if (missing(measure.vars)) {
     measure.vars <- setdiff(varnames, id.vars)
   }
-
+  
   list(id = id.vars, measure = measure.vars)
 }
